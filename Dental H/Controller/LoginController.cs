263bbbe234f07afba3fe858434609a1acfcae79c
TheManiDr <manuelviba91@gmail.com>
2026-05-19 01:09:41 -0600

    creacion de LoginController

---
+++ Dental H/Controller/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Dental_H.DAO;
+using Dental_H.Model;
+
+namespace Dental_H.Controller
+{
+    public class LoginController
+    {
+        private UsuarioDAO usuarioDAO;
+
+        public LoginController()
+        {
+            usuarioDAO = new UsuarioDAO();
+        }
+
+        public Usuario IniciarSesion(string usuario,string contrasena)
+        {
+            // VALIDAR CAMPOS VACÍOS
+            if (string.IsNullOrWhiteSpace(usuario) || string.IsNullOrWhiteSpace(contrasena))
+            {
+                return null;
+            }
+
+            // LLAMAR DAO
+            Usuario usuarioEncontrado = usuarioDAO.Login(usuario, contrasena);
+
+            return usuarioEncontrado;
+        }
+    }
+}
