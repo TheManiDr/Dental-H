eb89af7008c2e616dfb4c03e196dcdb5e73cd810
TheManiDr <manuelviba91@gmail.com>
2026-05-18 19:07:25 -0600

    Creacion de UsuarioDAO

---
+++ Dental H/DAO/UsuarioDAO.cs
@@ -0,0 +1,70 @@
+﻿using Dental_H.Model;
+using Dental_H.Util;
+using MySql.Data.MySqlClient;
+
+namespace Dental_H.DAO
+{
+    public class UsuarioDAO
+    {
+        public Usuario Login(string usuario, string contrasena)
+        {
+            Usuario usuarioEncontrado = null;
+
+            MySqlConnection conexion = Conexion.obtenerConexion();
+
+            try
+            {
+                conexion.Open();
+
+                string consulta =
+                    "SELECT * FROM Usuario " +
+                    "WHERE usuario = @usuario " +
+                    "AND contraseña = @contrasena";
+
+                MySqlCommand comando =
+                    new MySqlCommand(consulta, conexion);
+
+                comando.Parameters.AddWithValue(
+                    "@usuario",
+                    usuario
+                );
+
+                comando.Parameters.AddWithValue(
+                    "@contrasena",
+                    contrasena
+                );
+
+                MySqlDataReader reader =
+                    comando.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    usuarioEncontrado = new Usuario();
+
+                    usuarioEncontrado.IdUsuario =
+                        reader.GetInt32("id_usuario");
+
+                    usuarioEncontrado.NombreUsuario =
+                        reader.GetString("usuario");
+
+                    usuarioEncontrado.Contrasena =
+                        reader.GetString("contraseña");
+
+                    usuarioEncontrado.IdPersonal =
+                        reader.GetInt32("id_personal");
+
+                    usuarioEncontrado.IdRol =
+                        reader.GetInt32("id_rol");
+                }
+
+                conexion.Close();
+            }
+            catch
+            {
+                conexion.Close();
+            }
+
+            return usuarioEncontrado;
+        }
+    }
+}
