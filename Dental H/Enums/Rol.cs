2f3cef81d215c2fd9a171dcc9d6ba148ce3676e0
TheManiDr <manuelviba91@gmail.com>
2026-05-22 07:50:53 -0600

    rol.cs

---
+++ Dental H/Enums/Rol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental_H.Enums
+{
+    public enum Rol
+    {
+        ADMINISTRADOR = 1,
+        ODONTOLOGO = 2,
+        ASISTENTE = 3
+    }
+}
